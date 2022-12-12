e7d636fc7cb442407a688066642a15c002ac3a26
dumh <dumh>
2022-12-12 16:25:28 +0800

    1. add features

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,27 @@
+#[cfg(feature = "cell")]
 pub mod cell;
+
+#[cfg(feature = "mysqlx")]
 pub mod mysqlx;
+#[cfg(feature = "mysqlx")]
 mod mysqlx_test_pool;
+
+#[cfg(feature = "qh")]
 pub mod qh;
+
+#[cfg(feature = "sizehmap")]
 pub mod sizehmap;
+
+#[cfg(feature = "timer")]
 pub mod timer;
+
+#[cfg(feature = "ulog")]
 pub mod ulog;
+
+#[cfg(feature = "yaml")]
 pub mod yaml;
+
+#[cfg(feature = "ymdhms")]
 pub mod ymdhms;
 
 #[cfg(test)]
