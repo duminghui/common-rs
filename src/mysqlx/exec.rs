d4f081347e544bdb8f0cccd6bbd738f98ac49e1f
dumh <duminghui@users.noreply.github.com>
2023-06-20 20:54:40 +0800

    1. fix format

--- src/mysqlx/exec.rs
+++ src/mysqlx/exec.rs
@@ -24,7 +24,7 @@ pub struct ExecInfo {
 impl std::fmt::Display for ExecInfo {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!(
-            "Rows affected:{:4} [{:15?}]",
+            "Rows affected:{:5} [{:>12?}]",
             self.rows_affected, self.elapsed
         ))
     }
